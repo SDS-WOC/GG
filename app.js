7062252f25eb475c09f023634038ce905beb6959
Vishal <vishalg6700@gmail.com>
2017-12-31 19:16:32 +0530

    community buttons added

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var session = require("express-session")({
     resave: true,
     saveUninitialized: true
 });
+var bcrypt = require('bcrypt');
 games = ["Dota","AOE","League","CounterStrike","FIFA"];
+pages = ["login","signup"];
 
 server.listen(port, function () {
   console.log('Magic is happening at port %d', port);
@@ -88,10 +90,15 @@ updateUsernames();
 
 app.get('/home/:page',function(req,res){
    page = req.params.page;
+if(pages.includes(page))
+{
    if(req.session.user){
      res.redirect('/');
       } else {
         res.render(page);
+      }
+    }else{
+      res.redirect('/');
       }
   });
 
@@ -121,43 +128,75 @@ app.post('/myaction', function(req, res) {
   req.getConnection(function(err,connection){
 	console.log('req.body');
 	console.log(req.body);
-	var record = {username: req.body.username, email: req.body.email, password: req.body.password};
-
+	var record = {username: req.body.username, email: req.body.email, password:bcrypt.hashSync(req.body.password, 10)};
+var selectString = 'SELECT COUNT(username) FROM users WHERE username="'+[req.body.username]+'"';
+connection.query(selectString, function(err, results) {
+  var string=JSON.stringify(results);
+   if (string === '[{"COUNT(username)":0}]')  {
 	//connection.connect();
-  connection.query('INSERT INTO users SET username=? , email = ?, password = ?' ,[record['username'],record['email'], record['password']], function(err,res){
-          if(err) throw err;
+  connection.query('INSERT INTO users SET username=? , email = ?, password = ?' ,[record['username'],record['email'], record['password']], function(error,res){
+          if(error) throw error;
         console.log('Last record insert id:', res.insertId);
+        res.redirect('/home/login');
 	});
+}else{res.redirect('/home/signup');}
 });
-	res.redirect('/home/login');
-	//connection.end();
 
-res.end();
+	//connection.end();
+});
 });
 
 app.post('/verifyuser', function(req,res){
 req.getConnection(function(err,connection){
   console.log('checking user in database');
 	console.log(req.body.password);
-	var selectString = 'SELECT COUNT(username) FROM users WHERE username="'+req.body.username+'" AND password="'+req.body.password+'" ';
+var password = req.body.password;
+  connection.query('SELECT * FROM users WHERE username = ?',[req.body.username], function (error, results, fields) {
+console.log(results);
+if (error) {
+      res.redirect('/home/login');
+}else{
 
-	connection.query(selectString, function(err, results) {
-
-        console.log(results);
-        var string=JSON.stringify(results);
-        console.log(string);
-        //this is a walkaround of checking if the email pass combination is 1 or not it will fail if wrong pass is given
-        if (string === '[{"COUNT(username)":1}]') {
+  if(results.length >0){
+        bcrypt.compare(req.body.password, results[0].password ,function(err, ress) {
+     if(!ress){
+       res.redirect('/home/login');
+      }else{
       req.session.user = { hi : req.body.username };
       res.redirect('/');
-
-	        }
-        if (string === '[{"COUNT(username)":0}]')  {
-        	res.redirect('/home/login');
-
-        }
+    }
+  });
+}else{
+  res.redirect('/home/login');
+}
+}
 });
 });
+});
+
+
+app.post('/addcommunity',function(req,res){
+req.getConnection(function(err,connection){
+  console.log(req.body.lol);
+  connection.query('UPDATE users SET '+[req.body.lol]+'=1 WHERE username="'+[req.session.user.hi]+'"')
+if(err) throw err;
+});
+res.redirect('/');
+res.end();
+});
+app.get('/logout', function(req, res){
+   req.session.destroy();
+  res.redirect('/');
+});
+
+app.post('/leavecommunity',function(req,res){
+req.getConnection(function(err,connection){
+  console.log(req.body.lul);
+  connection.query('UPDATE users SET '+[req.body.lul]+'=0 WHERE username="'+[req.session.user.hi]+'"')
+if(err) throw err;
+});
+res.redirect('/');
+res.end();
 });
 app.get('/logout', function(req, res){
    req.session.destroy();
